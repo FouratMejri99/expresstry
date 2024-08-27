8850de3da749e5019a780cf0f23ab0c6c926a65f
Fourat Mejri <147564639+FouratMejri99@users.noreply.github.com>
2024-08-27 12:21:12 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/api', productRoutes);
 
 // Start the server
-const PORT = 3003;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
